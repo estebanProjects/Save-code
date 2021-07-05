2fe4f17bf8aba546d1d7ca25c3806bfdb598a514
estebanProjects <estebanespinoza1905@gmail.com>
2021-07-05 15:47:24 -0500

    subir git

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index.html', {title: 'Preguntas Pucp'});
+});
+
+router.get('/aritmetica', (req, res) => {
+    res.render('aritmetica.html', {title: 'Preguntas Aritmetica'});    
+});
+
+router.get('/algebra', (req, res) => {
+    res.render('algebra.html', {title: 'Preguntas Algebra'});    
+});
+
+router.get('/geometria', (req, res) => {
+    res.render('geometria.html', {title: 'Preguntas Geometria'});    
+});
+
+router.get('/matematica', (req, res) => {
+    res.render('matematica.html', {title: 'Preguntas Matematica'});    
+});
+
+module.exports = router;
